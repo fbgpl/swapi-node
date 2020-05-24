c757a5625682cc2dea963e7693abece01d4ae0ef
medynski <wojciech@medynski.io>
2020-05-24 23:22:50 +0200

    Implemented generic api paths

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 import cors from 'cors';
 import express from 'express';
 import http from 'http';
-import {
-  films,
-  people,
-  planets,
-  species,
-  starships,
-  transport,
-  vehicles,
-} from './fixtures/index.js';
+import { prepareFixtures } from './fixtures/index.js';
 import { deepClone } from './utils/deepClone.js';
-import { orderedObject } from './utils/orderedObject.js';
 
 const app = express();
 const httpServer = http.Server(app);
@@ -19,6 +10,8 @@ const httpServer = http.Server(app);
 // project constants
 const PORT = process.env.PORT || 3000;
 const ROWS_PER_PAGE = 10;
+const API_PATH = '/api/';
+const fixtures = prepareFixtures(API_PATH);
 
 // express app config
 app.use(cors());
@@ -30,62 +23,6 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-const convertRelations = (fields) => {
-  const output = deepClone(fields);
-
-  if (output.starships) {
-    output.starships = output.starships.map((id) => `/api/starships/${id}`);
-  }
-
-  if (output.vehicles) {
-    output.vehicles = output.vehicles.map((id) => `/api/vehicles/${id}`);
-  }
-
-  if (output.planets) {
-    output.planets = output.planets.map((id) => `/api/planets/${id}`);
-  }
-
-  if (output.characters) {
-    output.characters = output.characters.map((id) => `/api/people/${id}`);
-  }
-
-  if (output.species) {
-    output.species = output.species.map((id) => `/api/species/${id}`);
-  }
-
-  if (output.people) {
-    output.people = output.people.map((id) => `/api/people/${id}`);
-  }
-
-  if (output.pilots) {
-    output.pilots = output.pilots.map((id) => `/api/people/${id}`);
-  }
-
-  if (output.homeworld) {
-    output.homeworld = `/api/planets/${output.homeworld}`;
-  }
-
-  if (output.residents) {
-    output.residents = output.residents.map((id) => `/api/people/${id}`);
-  }
-
-  if (output.films) {
-    output.films = output.films.map((id) => `/api/films/${id}`);
-  }
-
-  return output;
-};
-
-const prepareResults = (pageData, dataUrl) => {
-  return pageData.map((data) => {
-    data.fields.url = `/api/${dataUrl}/${data.pk}`;
-    delete data.model;
-    delete data.pk;
-
-    return convertRelations(orderedObject(data.fields));
-  });
-};
-
 const preparePageData = (req, data, dataUrl) => {
   let page = 1;
   if (req.query && req.query.page) {
@@ -94,94 +31,27 @@ const preparePageData = (req, data, dataUrl) => {
 
   const count = data.length;
   const pages = Math.ceil(count / ROWS_PER_PAGE);
-  const next = pages > page ? `/api/${dataUrl}?page=${page + 1}` : null;
-  const previous = page > 1 ? `/api/${dataUrl}?page=${page - 1}` : null;
+  const next = pages > page ? `${API_PATH}${dataUrl}?page=${page + 1}` : null;
+  const previous = page > 1 ? `${API_PATH}${dataUrl}?page=${page - 1}` : null;
 
   const startsFrom = page * ROWS_PER_PAGE - ROWS_PER_PAGE;
-  const pageData = deepClone(data).splice(startsFrom, ROWS_PER_PAGE);
-
-  const results = prepareResults(pageData, dataUrl);
+  const results = deepClone(data).splice(startsFrom, ROWS_PER_PAGE);
 
   return { count, pages, next, previous, results };
 };
 
 const prepareSingleResult = (data, dataUrl, id) => {
-  const pageData = deepClone(data);
-  const results = prepareResults(pageData, dataUrl);
-  return results.find((res) => res.url === `/api/${dataUrl}/${id}`);
+  return data.find((res) => res.fields.url === `${API_PATH}${dataUrl}/${id}`);
 };
 
-app.get('/api/films', (req, res) => {
-  res.json(preparePageData(req, films, 'films'));
+app.get(`${API_PATH}:fixtureName`, (req, res) => {
+  const { fixtureName } = req.params;
+  res.json(preparePageData(req, fixtures[fixtureName], fixtureName));
 });
 
-app.get('/api/films/:id', (req, res) => {
-  res.json(prepareSingleResult(films, 'films', req.params.id));
-});
-
-app.get('/api/people', (req, res) => {
-  people.forEach((data) => {
-    data.fields.species = species
-      .filter((space) => space.fields.people.includes(data.pk))
-      .map((space) => space.pk);
-
-    data.fields.films = films
-      .filter((film) => film.fields.planets.includes(data.pk))
-      .map((film) => film.pk);
-
-    data.fields.vehicles = vehicles
-      .filter((vehicle) => vehicle.fields.pilots.includes(data.pk))
-      .map((vehicle) => vehicle.pk);
-
-    data.fields.starships = starships
-      .filter((starship) => starship.fields.pilots.includes(data.pk))
-      .map((starship) => starship.pk);
-  });
-
-  res.json(preparePageData(req, people, 'people'));
-});
-
-app.get('/api/planets', (req, res) => {
-  planets.forEach((data) => {
-    data.fields.residents = people
-      .filter((character) => character.fields.homeworld === data.pk)
-      .map((character) => character.pk);
-
-    data.fields.films = films
-      .filter((film) => film.fields.planets.includes(data.pk))
-      .map((film) => film.pk);
-  });
-
-  res.json(preparePageData(req, planets, 'planets'));
-});
-
-app.get('/api/species', (req, res) => {
-  res.json(preparePageData(req, species, 'species'));
-});
-
-app.get('/api/starships', (req, res) => {
-  starships.forEach((data) => {
-    data.fields.films = films
-      .filter((film) => film.fields.starships.includes(data.pk))
-      .map((film) => film.pk);
-  });
-
-  res.json(preparePageData(req, starships, 'starships'));
-});
-
-app.get('/api/vehicles', (req, res) => {
-  vehicles.forEach((data) => {
-    data.fields.films = films
-      .filter((film) => film.fields.vehicles.includes(data.pk))
-      .map((film) => film.pk);
-
-    data.fields = {
-      ...data.fields,
-      ...transport.find((transport) => transport.pk === data.pk).fields,
-    };
-  });
-
-  res.json(preparePageData(req, vehicles, 'vehicles'));
+app.get(`${API_PATH}:fixtureName/:id`, (req, res) => {
+  const { fixtureName, id } = req.params;
+  res.json(prepareSingleResult(fixtures[fixtureName], fixtureName, id));
 });
 
 httpServer.listen(PORT, () => {
--- fixtures/index.js
+++ fixtures/index.js
@@ -1,3 +1,5 @@
+import { deepClone } from './../utils/deepClone.js';
+import { orderedObject } from './../utils/orderedObject.js';
 import films from './films.js';
 import people from './people.js';
 import planets from './planets.js';
@@ -6,4 +8,153 @@ import starships from './starships.js';
 import transport from './transport.js';
 import vehicles from './vehicles.js';
 
-export { people, films, planets, species, starships, transport, vehicles };
+const fixtures = {
+  people,
+  films,
+  planets,
+  species,
+  starships,
+  transport,
+  vehicles,
+};
+
+const convertRelations = (fields, apiPath) => {
+  const output = deepClone(fields);
+
+  if (output.starships) {
+    output.starships = output.starships.map(
+      (id) => `${apiPath}starships/${id}`
+    );
+  }
+
+  if (output.vehicles) {
+    output.vehicles = output.vehicles.map((id) => `${apiPath}vehicles/${id}`);
+  }
+
+  if (output.planets) {
+    output.planets = output.planets.map((id) => `${apiPath}planets/${id}`);
+  }
+
+  if (output.characters) {
+    output.characters = output.characters.map((id) => `${apiPath}people/${id}`);
+  }
+
+  if (output.species) {
+    output.species = output.species.map((id) => `${apiPath}species/${id}`);
+  }
+
+  if (output.people) {
+    output.people = output.people.map((id) => `${apiPath}people/${id}`);
+  }
+
+  if (output.pilots) {
+    output.pilots = output.pilots.map((id) => `${apiPath}people/${id}`);
+  }
+
+  if (output.homeworld) {
+    output.homeworld = `${apiPath}planets/${output.homeworld}`;
+  }
+
+  if (output.residents) {
+    output.residents = output.residents.map((id) => `${apiPath}people/${id}`);
+  }
+
+  if (output.films) {
+    output.films = output.films.map((id) => `${apiPath}films/${id}`);
+  }
+
+  return output;
+};
+
+const sortAndClearResults = (fixturesData, dataUrl, apiPath) => {
+  return fixturesData.map((data) => {
+    data.fields.url = `${apiPath}${dataUrl}/${data.pk}`;
+    delete data.model;
+    delete data.pk;
+
+    return orderedObject(data.fields);
+  });
+};
+
+const prepareFixtures = (apiPath = '/') => {
+  // films
+  fixtures.films.forEach(
+    (data) => (data.fields = convertRelations(data.fields, apiPath))
+  );
+
+  // people
+  fixtures.people.forEach((data) => {
+    data.fields.species = fixtures.species
+      .filter((space) => space.fields.people.includes(data.pk))
+      .map((space) => space.pk);
+
+    data.fields.films = fixtures.films
+      .filter((film) => film.fields.planets.includes(data.pk))
+      .map((film) => film.pk);
+
+    data.fields.vehicles = fixtures.vehicles
+      .filter((vehicle) => vehicle.fields.pilots.includes(data.pk))
+      .map((vehicle) => vehicle.pk);
+
+    data.fields.starships = fixtures.starships
+      .filter((starship) => starship.fields.pilots.includes(data.pk))
+      .map((starship) => starship.pk);
+
+    data.fields = convertRelations(data.fields, apiPath);
+  });
+
+  // planets
+  fixtures.planets.forEach((data) => {
+    data.fields.residents = fixtures.people
+      .filter((character) => character.fields.homeworld === data.pk)
+      .map((character) => character.pk);
+
+    data.fields.films = fixtures.films
+      .filter((film) => film.fields.planets.includes(data.pk))
+      .map((film) => film.pk);
+
+    data.fields = convertRelations(data.fields, apiPath);
+  });
+
+  // species
+  fixtures.species.forEach(
+    (data) => (data.fields = convertRelations(data.fields, apiPath))
+  );
+
+  // starships
+  fixtures.starships.forEach((data) => {
+    data.fields.films = fixtures.films
+      .filter((film) => film.fields.starships.includes(data.pk))
+      .map((film) => film.pk);
+
+    data.fields = convertRelations(data.fields, apiPath);
+  });
+
+  // transport
+  fixtures.transport.forEach(
+    (data) => (data.fields = convertRelations(data.fields, apiPath))
+  );
+
+  // vehicles
+  fixtures.vehicles.forEach((data) => {
+    data.fields.films = fixtures.films
+      .filter((film) => film.fields.vehicles.includes(data.pk))
+      .map((film) => film.pk);
+
+    data.fields = {
+      ...data.fields,
+      ...fixtures.transport.find((transport) => transport.pk === data.pk)
+        .fields,
+    };
+
+    data.fields = convertRelations(data.fields, apiPath);
+  });
+
+  Object.keys(fixtures).forEach((key) => {
+    sortAndClearResults(fixtures[key], key, apiPath);
+  });
+
+  return fixtures;
+};
+
+export { prepareFixtures };
